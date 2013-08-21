aeee38feca05a4c12f1bab5ade6892dccf115dac
alcesleo <lagginglion@gmail.com>
2013-08-21 15:53:50 +0200

    Using gnu ln-command

--- dottify.sh
+++ dottify.sh
@@ -4,15 +4,16 @@
 scriptdir=$(dirname $0)
 
 # Loop the files in ./home
+# this requires coreutils to be installed,
+# if they are not, -n should be changed to -h
 for file in $scriptdir/home/*;
 do
     # The filename with a prepended dot
     dotfile=.${file##*/}
 
     # Make the link
-    ln -Ffhs $file ~/$dotfile
+    ln -Ffns $file ~/$dotfile
 done
 
 # link the bin folder
-ln -fhs $scriptdir/bin ~/.bin
-
+ln -fns $scriptdir/bin ~/.bin
