308c7a7159ed2d85bc106b55627889a33bfcfbb2
alcesleo <lagginglion@gmail.com>
2013-08-24 12:21:35 +0200

    link-dotfiles.sh can now be run fron anywhere

--- link-dotfiles.sh
+++ link-dotfiles.sh
@@ -1,19 +1,17 @@
 #!/bin/zsh
 
 # Get the path of this file
-scriptdir=$(dirname $0)
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
 
 # Loop the files in ./home
-# this requires coreutils to be installed,
-# if they are not, -n should be changed to -h
-for file in $scriptdir/home/*;
+for file in $ABSPATH/home/*;
 do
     # The filename with a prepended dot
     dotfile=.${file##*/}
 
     # Make the link
-    ln -Ffns $file ~/$dotfile
+    ln -Ffhs $file ~/$dotfile
 done
 
 # link the bin folder
-ln -fns $scriptdir/bin ~/.bin
+ln -fhs $ABSPATH/bin ~/.bin
