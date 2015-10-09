875c66d91d210ce8aec326c00127820da8d6e948
Jimmy Börjesson <lagginglion@gmail.com>
2015-10-09 09:30:36 +0200

    Enable more Engelbart mappings

--- karabiner.sh
+++ karabiner.sh
@@ -2,18 +2,24 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
+$cli set remap.engelbart_option 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set remap.engelbart_delete 1
+/bin/echo -n .
+$cli set remap.engelbart_control 1
+/bin/echo -n .
 $cli set option.emacsmode_controlH 1
 /bin/echo -n .
 $cli set repeat.wait 25
 /bin/echo -n .
-$cli set repeat.initial_wait 250
-/bin/echo -n .
-$cli set remap.controlJ2enter 1
-/bin/echo -n .
-$cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
 $cli set remap.commandR2commandR_enter 1
 /bin/echo -n .
 $cli set remap.engelbart_escape 1
+/bin/echo -n .
+$cli set remap.controlL2controlL_escape 1
+/bin/echo -n .
+$cli set remap.controlJ2enter 1
 /bin/echo -n .
 /bin/echo
