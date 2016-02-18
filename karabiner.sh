d5d9094f11c93a7be4ed8e3fdc890ab09cba9ba8
Jimmy Börjesson <alcesleo@gmail.com>
2016-02-18 12:57:18 +0100

    Center mouse on top window with fn

--- karabiner.sh
+++ karabiner.sh
@@ -10,6 +10,8 @@ $cli set remap.engelbart_delete 1
 /bin/echo -n .
 $cli set option.vimode_fn_hjkl 1
 /bin/echo -n .
+$cli set remap.move_mouse_cursor_center_fn_alone 1
+/bin/echo -n .
 $cli set remap.engelbart_control 1
 /bin/echo -n .
 $cli set option.emacsmode_controlH 1
