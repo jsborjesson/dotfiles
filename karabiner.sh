1353cb3973d890388f10da2e1e9071b8bdb63496
Jimmy Börjesson <alcesleo@gmail.com>
2016-03-21 09:39:50 +0100

    Enable mouse keys hjkl when holding d
    Faster by holding df, click with dv

--- karabiner.sh
+++ karabiner.sh
@@ -8,9 +8,9 @@ $cli set repeat.initial_wait 250
 /bin/echo -n .
 $cli set remap.engelbart_delete 1
 /bin/echo -n .
-$cli set option.vimode_fn_hjkl 1
-/bin/echo -n .
 $cli set remap.move_mouse_cursor_center_fn_alone 1
+/bin/echo -n .
+$cli set option.vimode_fn_hjkl 1
 /bin/echo -n .
 $cli set remap.engelbart_control 1
 /bin/echo -n .
@@ -25,5 +25,7 @@ $cli set remap.engelbart_escape 1
 $cli set remap.controlL2controlL_escape 1
 /bin/echo -n .
 $cli set remap.controlJ2enter 1
+/bin/echo -n .
+$cli set remap.mouse_keys_mode_2 1
 /bin/echo -n .
 /bin/echo
