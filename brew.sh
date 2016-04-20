88b944b690c6c2cbe60519ef124d64adef7abe0e
Jimmy Börjesson <alcesleo@gmail.com>
2016-04-20 14:51:05 +0200

    Map Fn+letters to special characters

--- brew.sh
+++ brew.sh
@@ -38,6 +38,7 @@ brew cask install google-chrome
 brew cask install iterm2
 brew cask install karabiner
 brew cask install keepassx
+brew cask install seil
 brew cask install spectacle
 brew cask install the-unarchiver
 
