08c71daa2d30c5d86b929097407601ef4655de55
Jimmy Börjesson <alcesleo@gmail.com>
2016-04-20 15:50:34 +0200

    Turn off unused Karabiner settings

--- karabiner.sh
+++ karabiner.sh
@@ -8,15 +8,13 @@ $cli set remap.engelbart_delete 1
 /bin/echo -n .
 $cli set repeat.initial_wait 250
 /bin/echo -n .
+$cli set remap.custom_fn_characters 1
+/bin/echo -n .
 $cli set remap.custom_tab_enter 1
 /bin/echo -n .
 $cli set remap.move_mouse_cursor_center_fn_alone 1
 /bin/echo -n .
-$cli set option.vimode_fn_hjkl 1
-/bin/echo -n .
 $cli set remap.engelbart_control 1
-/bin/echo -n .
-$cli set option.emacsmode_controlH 1
 /bin/echo -n .
 $cli set repeat.wait 20
 /bin/echo -n .
@@ -24,14 +22,8 @@ $cli set remap.simple_vi_mode 1
 /bin/echo -n .
 $cli set remap.engelbart_escape 1
 /bin/echo -n .
-$cli set remap.controlL2controlL_escape 1
-/bin/echo -n .
 $cli set remap.custom_cmd 1
 /bin/echo -n .
-$cli set remap.controlJ2enter 1
-/bin/echo -n .
 $cli set remap.mouse_keys_mode_2 1
-/bin/echo -n .
-$cli set remap.custom_fn_characters 1
 /bin/echo -n .
 /bin/echo
