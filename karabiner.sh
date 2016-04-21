122ecde0b5a11ead910ccda71551040d015034d0
Jimmy Börjesson <alcesleo@gmail.com>
2016-04-21 15:44:36 +0200

    Add custom engelbart shift combinations

--- karabiner.sh
+++ karabiner.sh
@@ -8,22 +8,24 @@ $cli set remap.engelbart_delete 1
 /bin/echo -n .
 $cli set repeat.initial_wait 250
 /bin/echo -n .
-$cli set remap.custom_fn_characters 1
-/bin/echo -n .
 $cli set remap.custom_tab_enter 1
 /bin/echo -n .
 $cli set remap.move_mouse_cursor_center_fn_alone 1
+/bin/echo -n .
+$cli set option.vimode_fn_hjkl 1
 /bin/echo -n .
 $cli set remap.engelbart_control 1
 /bin/echo -n .
 $cli set repeat.wait 20
 /bin/echo -n .
-$cli set option.vimode_fn_hjkl 1
+$cli set remap.custom_shift 1
 /bin/echo -n .
 $cli set remap.engelbart_escape 1
 /bin/echo -n .
 $cli set remap.custom_cmd 1
 /bin/echo -n .
 $cli set remap.mouse_keys_mode_2 1
+/bin/echo -n .
+$cli set remap.custom_fn_characters 1
 /bin/echo -n .
 /bin/echo
