bc1c9c85e22cd4a0639b64339763aa142b6dd0f5
Jimmy Börjesson <alcesleo@gmail.com>
2016-05-13 20:42:06 +0200

    Install emacs packages automatically

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,6 +1,23 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
+
+(setq package-list
+      '(
+	evil
+	))
+
 (package-initialize)
+
+(or
+ (file-exists-p package-user-dir)
+ (package-refresh-contents))
+
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(dolist (package package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 (require 'evil)
 (evil-mode t)
