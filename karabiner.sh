e5877652b6836826b3d218f73c32166a45a9da0f
Jimmy Börjesson <alcesleo@gmail.com>
2016-09-06 13:28:53 +0200

    Update karabiner.sh

--- karabiner.sh
+++ karabiner.sh
@@ -2,22 +2,24 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.initial_wait 250
+$cli set custom.keepass_login 1
 /bin/echo -n .
-$cli set remap.custom_ergonomic_modifiers 1
+$cli set remap.custom_ergonomic_arrow_keys 1
 /bin/echo -n .
 $cli set remap.custom_ergonomic_fringe_keys 1
 /bin/echo -n .
-$cli set remap.move_mouse_cursor_center_fn_alone 1
+$cli set remap.custom_ergonomic_modifiers 1
 /bin/echo -n .
-$cli set option.vimode_fn_hjkl 1
-/bin/echo -n .
-$cli set repeat.wait 20
+$cli set remap.custom_fn_characters 1
 /bin/echo -n .
 $cli set remap.custom_fn_shift_numbers 1
 /bin/echo -n .
 $cli set remap.mouse_keys_mode_2 1
 /bin/echo -n .
-$cli set remap.custom_fn_characters 1
+$cli set remap.move_mouse_cursor_center_fn_alone 1
+/bin/echo -n .
+$cli set repeat.initial_wait 250
+/bin/echo -n .
+$cli set repeat.wait 20
 /bin/echo -n .
 /bin/echo
