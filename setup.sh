a4e56b06cd826ba6abc813b56779c3aa734fcbf0
Jimmy Börjesson <alcesleo@gmail.com>
2019-01-27 11:16:23 +0100

    Move spectacle shortcuts to subfolder

--- setup.sh
+++ setup.sh
@@ -25,6 +25,6 @@ ln -sfnv $SCRIPT_PATH/ruby/railsrc ~/.railsrc
 ln -sfnv $SCRIPT_PATH/tmux.conf ~/.tmux.conf
 ln -sfnv $SCRIPT_PATH/vim ~/.vim
 
-ln -sfnv $SCRIPT_PATH/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
+ln -sfnv $SCRIPT_PATH/spectacle/Shortcuts.json ~/Library/Application\ Support/Spectacle/Shortcuts.json
 ln -sfnv $SCRIPT_PATH/karabiner/ ~/.config/
 ln -sfnv $SCRIPT_PATH/macos/ssh/config ~/.ssh/config
