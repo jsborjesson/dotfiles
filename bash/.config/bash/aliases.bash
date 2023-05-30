38c3423c07fbdc4d72232eb77de0e8a8e1aa9e5d
Jimmy Börjesson <jimmy.borjesson@gmail.com>
2023-05-30 09:36:50 +0200

    Modify col-shortcuts to work from the back too

--- bash/.config/bash/aliases.bash
+++ bash/.config/bash/aliases.bash
@@ -31,15 +31,24 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 
 # Shortcuts for getting columns
-alias col-1="awk '{print \$1}'"
-alias col-2="awk '{print \$2}'"
-alias col-3="awk '{print \$3}'"
-alias col-4="awk '{print \$4}'"
-alias col-5="awk '{print \$5}'"
-alias col-6="awk '{print \$6}'"
-alias col-7="awk '{print \$7}'"
-alias col-8="awk '{print \$8}'"
-alias col-9="awk '{print \$9}'"
+alias col-1="awk '{print \$NF}'"
+alias col-2="awk '{print \$NF-2}'"
+alias col-3="awk '{print \$NF-3}'"
+alias col-4="awk '{print \$NF-4}'"
+alias col-5="awk '{print \$NF-5}'"
+alias col-6="awk '{print \$NF-6}'"
+alias col-7="awk '{print \$NF-7}'"
+alias col-8="awk '{print \$NF-8}'"
+alias col-9="awk '{print \$NF-9}'"
+alias col1="awk '{print \$1}'"
+alias col2="awk '{print \$2}'"
+alias col3="awk '{print \$3}'"
+alias col4="awk '{print \$4}'"
+alias col5="awk '{print \$5}'"
+alias col6="awk '{print \$6}'"
+alias col7="awk '{print \$7}'"
+alias col8="awk '{print \$8}'"
+alias col9="awk '{print \$9}'"
 
 # Enable autocomplete for the g alias if available
 type __git_complete >/dev/null 2>&1 && {
