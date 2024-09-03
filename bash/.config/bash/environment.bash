1876ad463cd8a9db7b017c72be333968af9f0e4f
Jimmy Börjesson <jimmy.borjesson@gmail.com>
2024-09-03 14:28:13 +0200

    Make history work when starting tmux

--- bash/.config/bash/environment.bash
+++ bash/.config/bash/environment.bash
@@ -1,3 +1,6 @@
 export EDITOR='vim'
-export HISTCONTROL='erasedups'
 export PATH="$PATH:~/.bin"
+
+export PROMPT_COMMAND="history -a; history -r; $PROMPT_COMMAND"
+export HISTCONTROL='ignoredups:erasedups'
+shopt -s histappend
